d410da4bed574305cef3bbbf082cfc4f633851a5
Omar Kormadi <amorkormadi@gmail.com>
2025-01-21 09:44:44 +0100

    testing

---
+++ src/app/liste-films/liste-films.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-films',
+  standalone: true,
+  imports: [],
+  templateUrl: './liste-films.component.html',
+  styleUrl: './liste-films.component.css'
+})
+export class ListeFilmsComponent {
+
+}
