97b1ec9cbcdfdedfde3af7f49a32a75b453dd8ee
Omar Kormadi <amorkormadi@gmail.com>
2025-01-28 09:58:43 +0100

    comp1 and comp2 added with importing RouterModule in app.component.ts imports:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ListeFilmsComponent } from "./liste-films/liste-films.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListeFilmsComponent],
+  imports: [RouterModule ,RouterOutlet, ListeFilmsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
