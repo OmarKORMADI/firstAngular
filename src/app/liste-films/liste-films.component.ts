41af77f7595a0c621cc59e0471958012276cdfee
Omar Kormadi <amorkormadi@gmail.com>
2025-01-28 09:16:40 +0100

    boucle for added

--- src/app/liste-films/liste-films.component.ts
+++ src/app/liste-films/liste-films.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-liste-films',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './liste-films.component.html',
   styleUrl: './liste-films.component.css'
 })
