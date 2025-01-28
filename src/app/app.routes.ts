b44d0c6630f1f409c2d595192285f1ed0b2495f3
Omar Kormadi <amorkormadi@gmail.com>
2025-01-28 10:29:31 +0100

    erreur , comp1 comp2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { ErreurComponent } from './erreur/erreur.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"chemin1" , component : Comp1Component},
+    {path:"chemin2" , component : Comp2Component},
+    {path : '**', component : ErreurComponent}
+
+];
